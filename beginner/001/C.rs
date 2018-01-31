a3131256df950595fedcf55ef2be092883e8212a
Ittoh Kimura <kimura.itto.kd3@is.naist.jp>
2018-01-31 15:38:35 +0900

    [001] C

--- beginner/001/C.rs
+++ beginner/001/C.rs
@@ -10,8 +10,33 @@ fn read_int_pair() -> (i32, i32) {
     (inputs[0], inputs[1])
 }
 
+fn wind_power(dis: i32) -> i32 {
+    let dis = ((dis as f32) / 60.0_f32 * 10.0_f32).round();
+    let range = [
+        0.0, 0.2, 0.3, 1.5, 1.6, 3.3, 3.4, 5.4, 5.5, 7.9,
+        8.0, 10.7, 10.8, 13.8, 13.9, 17.1, 17.2, 20.7, 20.8, 24.4,
+        24.5, 28.4, 28.5, 32.6, 32.7,
+    ];
+    let range: Vec<f32> = range.iter().map(|x| x * 10_f32).collect();
+    for i in (0..range.len()-1).filter(|&x| x % 2 == 0) {
+        if range[i] <= dis && dis <= range[i+1] {
+            return (i as i32) / 2
+        }
+    }
+    12
+}
+
 fn main() {
+    let directions = [
+        "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE",
+        "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW",
+    ];
     let (deg, dis) = read_int_pair();
-    let wind_speed_average = (dis as f32) / 60.0_f32;
-    println!("{}", wind_speed_average);
+    let direction = ((deg * 10 + 1125) / 2250 % 16) as usize;
+    let w = wind_power(dis);
+    if w == 0 {
+        println!("C 0");
+    } else {
+        println!("{} {}", directions[direction], w);
+    }
 }
