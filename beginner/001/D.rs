bfc3eca6d7f11a8e69958767ce3918ee12ea6db4
Ittoh Kimura <kimura.itto.kd3@is.naist.jp>
2018-02-01 12:35:13 +0900

    Beginner[001] D

--- beginner/001/D.rs
+++ beginner/001/D.rs
@@ -19,10 +19,16 @@ fn read_int_pair() -> (i32, i32) {
 
 fn convert_timeint_to_int(timeint_range_list: &mut Vec<(i32, i32)>) -> Vec<(i32, i32)> {
     let mut int_range_list: Vec<(i32, i32)> = Vec::with_capacity(timeint_range_list.len());
-    for (i, &(start, end)) in timeint_range_list.iter().enumerate() {
+    for &(start, end) in timeint_range_list.iter() {
         let convert = |time: i32| -> i32 { time / 100 * 60 + time % 100 };
         let floor_5min_interval = |x: i32| -> i32 { x - (x % 5) };
-        let ceil_5min_interval = |x: i32| -> i32 { x + 5 - (x % 5) };
+        let ceil_5min_interval = |x: i32| -> i32 {
+            if x % 5 != 0 {
+                x + 5 - (x % 5)
+            } else {
+                x
+            }
+        };
         let start = floor_5min_interval(convert(start));
         let end = ceil_5min_interval(convert(end));
         int_range_list.push((start, end));
@@ -36,12 +42,27 @@ fn main() {
     for _ in 0..number_of_line {
         timeint_range_list.push(read_int_pair());
     }
-    println!("{:?}", timeint_range_list);
-    println!("{:?}", convert_timeint_to_int(&mut timeint_range_list));
     let mut timeline = [0_i32; 2400 * 60];
     for item in convert_timeint_to_int(&mut timeint_range_list).iter() {
-        for i in item.0..item.1 {
+        for i in item.0..item.1+1 {
             timeline[i as usize] = 1;
+        }
+    }
+
+    let convert_i32_to_timestr = |time_u: i32| -> String {
+        format!("{:02}{:02}", (time_u / 60), (time_u % 60))
+    };
+
+    let mut start: i32 = -1;
+    for (idx, item) in timeline.iter().enumerate() {
+        if *item == 1 && start == -1 {
+            start = idx as i32;
+        }
+        if *item == 0 && start != -1 {
+            println!("{}-{}",
+                     convert_i32_to_timestr(start),
+                     convert_i32_to_timestr((idx as i32) - 1));
+            start = -1;
         }
     }
 }
