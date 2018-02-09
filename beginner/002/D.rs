19bddfee88041a0b60803da0b5b06de1f3e9af50
Ittoh Kimura <kimura.itto.kd3@is.naist.jp>
2018-02-09 12:10:55 +0900

    [002] D

--- beginner/002/D.rs
+++ beginner/002/D.rs
@@ -1,8 +1,22 @@
+const N_MAX: usize = 12;
+
+fn i32_to_bitvec(integer: i32) -> Vec<bool> {
+    let mut v: Vec<bool> = Vec::new();
+    for i in 0..N_MAX {
+        if integer & (1 << i) > 0 {
+            v.push(true);
+        } else {
+            v.push(false);
+        }
+    }
+    v
+}
+
 #[allow(unused_must_use)]
-fn read_int_pair() -> (i32, i32) {
+fn read_usize_pair() -> (usize, usize) {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input);
-    let v: Vec<i32> = input
+    let v: Vec<usize> = input
         .trim()
         .split_whitespace()
         .map(|x| x.parse().unwrap())
@@ -11,10 +25,27 @@ fn read_int_pair() -> (i32, i32) {
 }
 
 fn main() {
-    let (n, m) = read_int_pair();
-    let mut relations: Vec<(i32, i32)> = Vec::with_capacity(m as usize);
+    let (n, m) = read_usize_pair();
+    let mut relations = [[false; N_MAX]; N_MAX];
     for _ in 0..m {
-        relations.push(read_int_pair());
+        let (x, y) = read_usize_pair();
+        relations[x-1][y-1] = true;
+        relations[y-1][x-1] = true;
     }
-    println!("{:?}", relations);
+    let mut answer = 0;
+    'nextx: for x in 0..(1 << n) {
+        let bitvector = i32_to_bitvec(x);
+        let bool_to_i32 = |x: bool| -> i32 { if x { 1 } else { 0 } };
+        let number_of_1bit = bitvector.iter().fold(0, |acc, &x| acc + bool_to_i32(x));
+        if number_of_1bit <= answer { continue; }
+        for i in 0..n {
+            for j in 0..i {
+                if bitvector[i] && bitvector[j] && !relations[i][j] {
+                    continue 'nextx;
+                }
+            }
+        }
+        answer = number_of_1bit;
+    }
+    println!("{}", answer);
 }
