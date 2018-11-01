2794c38c7aacaaf592a8cb6fb85ad06e08f7b5ea
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2018-11-01 22:40:56 +0900

    [105] C

---
+++ ABC/105/C.rs
@@ -0,0 +1,83 @@
+macro_rules! input {
+    ($ttype:ty) => {
+        {
+            let mut input = String::new();
+            let _= std::io::stdin().read_line(&mut input);
+            input.trim().parse::<$ttype>().unwrap()
+        }
+    };
+
+    ($($ttype:ty),*) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let mut iter = input.trim().split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$ttype>().unwrap(),)*
+            )
+        }
+    };
+
+    ($ttype:ty; $x:expr) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let v: Vec<$ttype> = input
+                .trim()
+                .split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect();
+            v
+        }
+    };
+
+    ($ttype:ty; $x:expr; $y:expr) => {
+        {
+            use std::io::BufRead;
+            let mut matrix = Vec::with_capacity($y);
+            for _ in 0..$y {
+                let mut input = String::new();
+                let stdin = std::io::stdin();
+                let _ = stdin.lock().read_line(&mut input);
+                let v: Vec<$ttype> = input
+                    .trim()
+                    .split_whitespace()
+                    .map(|x| x.parse().unwrap())
+                    .collect();
+                matrix.push(v);
+            }
+            matrix
+        }
+    };
+}
+
+fn main() {
+    let mut n = input!(i64);
+    let mut i: u32 = 0;
+    let mut zero_or_one: Vec<i32> = Vec::with_capacity(64);
+
+    if n == 0 {
+        println!("0");
+        return;
+    }
+
+    while n != 0 {
+        if n % (-2i64).pow(i+1) != 0 {
+            zero_or_one.push(1);
+            n -= (-2i64).pow(i);
+        } else {
+            zero_or_one.push(0);
+        }
+        i += 1;
+    }
+
+    zero_or_one.reverse();
+    for bit in zero_or_one.iter() {
+        print!("{}", bit);
+    }
+    println!("");
+}
