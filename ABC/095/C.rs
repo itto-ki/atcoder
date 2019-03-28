145db9fbf06b17eb777902d8790d2314dd18d2a3
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2019-03-29 01:16:44 +0900

    [095] C

---
+++ ABC/095/C.rs
@@ -0,0 +1,78 @@
+macro_rules! input {
+    ($ttype:ty) => {
+        {
+            let mut input = String::new();
+            let _= std::io::stdin().read_line(&mut input);
+            input.trim().parse::<$ttype>().unwrap()
+        }
+    };
+
+    ($($ttype:ty),*) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let mut iter = input.trim().split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$ttype>().unwrap(),)*
+            )
+        }
+    };
+
+    ($ttype:ty; $x:expr) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let v: Vec<$ttype> = input
+                .trim()
+                .split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect();
+            v
+        }
+    };
+
+    ($ttype:ty; $x:expr; $y:expr) => {
+        {
+            use std::io::BufRead;
+            let mut matrix = Vec::with_capacity($y);
+            for _ in 0..$y {
+                let mut input = String::new();
+                let stdin = std::io::stdin();
+                let _ = stdin.lock().read_line(&mut input);
+                let v: Vec<$ttype> = input
+                    .trim()
+                    .split_whitespace()
+                    .map(|x| x.parse().unwrap())
+                    .collect();
+                matrix.push(v);
+            }
+            matrix
+        }
+    };
+}
+
+fn main() {
+    let (mut a, mut b, c,  mut x, mut y) = input!(i32, i32, i32, i32, i32);
+    let mut total = 0;
+
+    while x > 0 || y > 0 {
+        if x <= 0 {
+            a = 0;
+        }
+        if y <= 0 {
+            b = 0;
+        }
+        if a + b > c * 2 {
+            total += c * 2;
+        } else {
+            total += a + b;
+        }
+        x -= 1;
+        y -= 1;
+    }
+    println!("{}", total);
+}
