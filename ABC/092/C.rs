52f6aa9683b91c32cbe27e52db49001b8a39e0db
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2019-03-29 22:07:21 +0900

    [092] C

---
+++ ABC/092/C.rs
@@ -0,0 +1,76 @@
+macro_rules! input {
+    ($ttype:ty) => {
+        {
+            let mut input = String::new();
+            let _= std::io::stdin().read_line(&mut input);
+            input.trim().parse::<$ttype>().unwrap()
+        }
+    };
+
+    ($($ttype:ty),*) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let mut iter = input.trim().split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$ttype>().unwrap(),)*
+            )
+        }
+    };
+
+    ($ttype:ty; $x:expr) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let v: Vec<$ttype> = input
+                .trim()
+                .split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect();
+            v
+        }
+    };
+
+    ($ttype:ty; $x:expr; $y:expr) => {
+        {
+            use std::io::BufRead;
+            let mut matrix = Vec::with_capacity($y);
+            for _ in 0..$y {
+                let mut input = String::new();
+                let stdin = std::io::stdin();
+                let _ = stdin.lock().read_line(&mut input);
+                let v: Vec<$ttype> = input
+                    .trim()
+                    .split_whitespace()
+                    .map(|x| x.parse().unwrap())
+                    .collect();
+                matrix.push(v);
+            }
+            matrix
+        }
+    };
+}
+
+fn main() {
+    let n = input!(usize);
+    let mut a = input!(i32; n);
+
+    a.insert(0, 0);
+    a.push(0);
+
+    let mut total = 0;
+    for i in 0..n+1 {
+        total += (a[i+1] - a[i]).abs();
+    }
+
+    for i in 1..n+1 {
+        let before = (a[i] - a[i-1]).abs() + (a[i+1] - a[i]).abs();
+        let after = (a[i+1] - a[i-1]).abs();
+        let delta = before - after;
+        println!("{}", total - delta);
+    }
+}
