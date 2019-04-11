3d905f83afe958f662bcd6ff0bdfe40f0693a656
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2019-04-12 02:06:51 +0900

    [081] C

---
+++ ABC/082/C.rs
@@ -0,0 +1,84 @@
+macro_rules! input {
+    ($ttype:ty) => {
+        {
+            let mut input = String::new();
+            let _= std::io::stdin().read_line(&mut input);
+            input.trim().parse::<$ttype>().unwrap()
+        }
+    };
+
+    ($($ttype:ty),*) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let mut iter = input.trim().split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$ttype>().unwrap(),)*
+            )
+        }
+    };
+
+    ($ttype:ty; $x:expr) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let v: Vec<$ttype> = input
+                .trim()
+                .split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect();
+            v
+        }
+    };
+
+    ($ttype:ty; $x:expr; $y:expr) => {
+        {
+            use std::io::BufRead;
+            let mut matrix = Vec::with_capacity($y);
+            for _ in 0..$y {
+                let mut input = String::new();
+                let stdin = std::io::stdin();
+                let _ = stdin.lock().read_line(&mut input);
+                let v: Vec<$ttype> = input
+                    .trim()
+                    .split_whitespace()
+                    .map(|x| x.parse().unwrap())
+                    .collect();
+                matrix.push(v);
+            }
+            matrix
+        }
+    };
+}
+
+use std::collections::HashMap;
+
+fn main() {
+    let _n = input!(i64);
+    let a_lst = input!(i64; n);
+    let mut map = HashMap::new();
+
+    for &a in a_lst.iter() {
+        if map.contains_key(&a) {
+            if let Some(x) = map.get_mut(&a) {
+                *x += 1;
+            }
+        } else {
+            map.insert(a, 1);
+        }
+    }
+
+    let mut cnt: i64 = 0;
+    for (key, value) in map.iter() {
+        if key > value {
+            cnt += *value;
+        } else if key < value {
+            cnt += value - key;
+        }
+    }
+    println!("{}", cnt);
+}
