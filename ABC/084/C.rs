b61008871887e587f419f371dcc7c5cdaaa611e4
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2019-04-11 12:25:51 +0900

    [084] C

---
+++ ABC/084/C.rs
@@ -0,0 +1,80 @@
+macro_rules! input {
+    ($ttype:ty) => {
+        {
+            let mut input = String::new();
+            let _= std::io::stdin().read_line(&mut input);
+            input.trim().parse::<$ttype>().unwrap()
+        }
+    };
+
+    ($($ttype:ty),*) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let mut iter = input.trim().split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$ttype>().unwrap(),)*
+            )
+        }
+    };
+
+    ($ttype:ty; $x:expr) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let v: Vec<$ttype> = input
+                .trim()
+                .split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect();
+            v
+        }
+    };
+
+    ($ttype:ty; $x:expr; $y:expr) => {
+        {
+            use std::io::BufRead;
+            let mut matrix = Vec::with_capacity($y);
+            for _ in 0..$y {
+                let mut input = String::new();
+                let stdin = std::io::stdin();
+                let _ = stdin.lock().read_line(&mut input);
+                let v: Vec<$ttype> = input
+                    .trim()
+                    .split_whitespace()
+                    .map(|x| x.parse().unwrap())
+                    .collect();
+                matrix.push(v);
+            }
+            matrix
+        }
+    };
+}
+
+fn main() {
+    let n = input!(usize);
+    let mut lst: Vec<(i32, i32, i32)> = Vec::with_capacity(n);
+    for _ in 0..n-1 {
+        lst.push(input!(i32, i32, i32));
+    }
+
+
+    for i in 0..n {
+        let mut t: i32 = 0;
+        for &(c, s, f) in lst.iter().skip(i) {
+            if t < s {
+                t = s;
+            } else if t % f == 0 {
+                t += 0;
+            } else {
+                t = t + f - (t % f);
+            }
+            t += c;
+        }
+        println!("{}", t);
+    }
+}
