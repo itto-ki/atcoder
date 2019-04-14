68eda297a6ec7c9f391096857ae82052ac741e1d
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2019-04-14 12:14:00 +0900

    [077] C

---
+++ ABC/077/C.rs
@@ -0,0 +1,109 @@
+macro_rules! input {
+    ($ttype:ty) => {
+        {
+            let mut input = String::new();
+            let _= std::io::stdin().read_line(&mut input);
+            input.trim().parse::<$ttype>().unwrap()
+        }
+    };
+
+    ($($ttype:ty),*) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let mut iter = input.trim().split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$ttype>().unwrap(),)*
+            )
+        }
+    };
+
+    ($ttype:ty; $x:expr) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let v: Vec<$ttype> = input
+                .trim()
+                .split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect();
+            v
+        }
+    };
+
+    ($ttype:ty; $x:expr; $y:expr) => {
+        {
+            use std::io::BufRead;
+            let mut matrix = Vec::with_capacity($y);
+            for _ in 0..$y {
+                let mut input = String::new();
+                let stdin = std::io::stdin();
+                let _ = stdin.lock().read_line(&mut input);
+                let v: Vec<$ttype> = input
+                    .trim()
+                    .split_whitespace()
+                    .map(|x| x.parse().unwrap())
+                    .collect();
+                matrix.push(v);
+            }
+            matrix
+        }
+    };
+}
+
+use std::cmp::Ordering;
+
+trait BinarySearch<T> {
+    fn upper_bound(&self, &T) -> usize;
+    fn lower_bound(&self, &T) -> usize;
+}
+
+impl<T: Ord> BinarySearch<T> for [T] {
+    fn upper_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less | Ordering::Equal => low = mid + 1,
+                Ordering::Greater => high = mid,
+            }
+        }
+        low
+    }
+
+    fn lower_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less => low = mid + 1,
+                Ordering::Equal | Ordering::Greater => high = mid,
+            }
+        }
+        low
+    }
+}
+
+fn main() {
+    let _n = input!(i64);
+    let mut a = input!(i64; n);
+    let b = input!(i64; n);
+    let mut c = input!(i64; n);
+
+    a.sort();
+    c.sort();
+
+    let mut cnt: usize = 0;
+    for x in b.iter() {
+        cnt += a.lower_bound(x) * (c.len() - c.upper_bound(x));
+    }
+    println!("{}", cnt);
+}
