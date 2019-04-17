559829ea11cf8bcefbea70341ab6f131a4d2ecc5
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2019-04-17 12:42:30 +0900

    [064] C

---
+++ ABC/064/C.rs
@@ -0,0 +1,80 @@
+macro_rules! input {
+    ($ttype:ty) => {
+        {
+            let mut input = String::new();
+            let _= std::io::stdin().read_line(&mut input);
+            input.trim().parse::<$ttype>().unwrap()
+        }
+    };
+
+    ($($ttype:ty),*) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let mut iter = input.trim().split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$ttype>().unwrap(),)*
+            )
+        }
+    };
+
+    ($ttype:ty; $x:expr) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let v: Vec<$ttype> = input
+                .trim()
+                .split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect();
+            v
+        }
+    };
+
+    ($ttype:ty; $x:expr; $y:expr) => {
+        {
+            use std::io::BufRead;
+            let mut matrix = Vec::with_capacity($y);
+            for _ in 0..$y {
+                let mut input = String::new();
+                let stdin = std::io::stdin();
+                let _ = stdin.lock().read_line(&mut input);
+                let v: Vec<$ttype> = input
+                    .trim()
+                    .split_whitespace()
+                    .map(|x| x.parse().unwrap())
+                    .collect();
+                matrix.push(v);
+            }
+            matrix
+        }
+    };
+}
+
+use std::collections::HashSet;
+
+fn main() {
+    let n = input!(usize);
+    let a_lst = input!(i64; n);
+    let mut set: HashSet<i64> = HashSet::new();
+
+    let mut pro_cnt = 0;
+    for &rate in a_lst.iter() {
+        if rate / 400 > 7 {
+            pro_cnt += 1;
+        } else {
+            set.insert(rate / 400);
+        }
+    }
+    let min = set.iter().count();
+    let max = min + pro_cnt;
+    if min == 0 {
+        println!("{} {}", 1, max);
+    } else {
+        println!("{} {}", min, max);
+    }
+}
