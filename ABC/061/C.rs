d0dfd5a6dfbbef6c1708fda58867cb989109e61b
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2019-04-18 19:29:51 +0900

    [061] C

---
+++ ABC/061/C.rs
@@ -0,0 +1,74 @@
+macro_rules! input {
+    ($ttype:ty) => {
+        {
+            let mut input = String::new();
+            let _= std::io::stdin().read_line(&mut input);
+            input.trim().parse::<$ttype>().unwrap()
+        }
+    };
+
+    ($($ttype:ty),*) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let mut iter = input.trim().split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$ttype>().unwrap(),)*
+            )
+        }
+    };
+
+    ($ttype:ty; $x:expr) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let v: Vec<$ttype> = input
+                .trim()
+                .split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect();
+            v
+        }
+    };
+
+    ($ttype:ty; $x:expr; $y:expr) => {
+        {
+            use std::io::BufRead;
+            let mut matrix = Vec::with_capacity($y);
+            for _ in 0..$y {
+                let mut input = String::new();
+                let stdin = std::io::stdin();
+                let _ = stdin.lock().read_line(&mut input);
+                let v: Vec<$ttype> = input
+                    .trim()
+                    .split_whitespace()
+                    .map(|x| x.parse().unwrap())
+                    .collect();
+                matrix.push(v);
+            }
+            matrix
+        }
+    };
+}
+
+fn main() {
+    let (n, k) = input!(usize, i64);
+    let mut vec: Vec<(i64, i64)> = Vec::with_capacity(n);
+    for _ in 0..n {
+        vec.push(input!(i64, i64));
+    }
+
+    let mut counter: i64 = 0;
+    vec.sort_by_key(|pair| pair.0);
+    for pair in vec.iter() {
+        counter += pair.1;
+        if counter >= k {
+            println!("{}", pair.0);
+            break;
+        }
+    }
+}
