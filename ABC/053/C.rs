e2c3367c8ad0961b35459067bd8c503fd57acb61
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2019-05-13 01:54:49 +0900

    [053] C

---
+++ ABC/053/C.rs
@@ -0,0 +1,73 @@
+macro_rules! input {
+    ($ttype:ty) => {
+        {
+            let mut input = String::new();
+            let _= std::io::stdin().read_line(&mut input);
+            input.trim().parse::<$ttype>().unwrap()
+        }
+    };
+
+    ($($ttype:ty),*) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let mut iter = input.trim().split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$ttype>().unwrap(),)*
+            )
+        }
+    };
+
+    ($ttype:ty; $x:expr) => {
+        {
+            use std::io::BufRead;
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            let _ = stdin.lock().read_line(&mut input);
+            let v: Vec<$ttype> = input
+                .trim()
+                .split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect();
+            v
+        }
+    };
+
+    ($ttype:ty; $x:expr; $y:expr) => {
+        {
+            use std::io::BufRead;
+            let mut matrix = Vec::with_capacity($y);
+            for _ in 0..$y {
+                let mut input = String::new();
+                let stdin = std::io::stdin();
+                let _ = stdin.lock().read_line(&mut input);
+                let v: Vec<$ttype> = input
+                    .trim()
+                    .split_whitespace()
+                    .map(|x| x.parse().unwrap())
+                    .collect();
+                matrix.push(v);
+            }
+            matrix
+        }
+    };
+}
+
+fn main() {
+    let x = input!(i64);
+
+    if x == 6 {
+        println!("1");
+    } else {
+        let result = x / 11;
+        if x % 11 == 0 {
+            println!("{}", result * 2);
+        } else if x % 11 <= 6 {
+            println!("{}", result * 2 + 1);
+        } else {
+            println!("{}", result * 2 + 2);
+        }
+    }
+}
