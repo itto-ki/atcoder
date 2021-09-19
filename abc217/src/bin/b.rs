eaa8f64edc485592d9a98e911b0bbdc520deb1de
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-09-19 21:39:49 +0900

    Feat: abc217 b

--- abc217/src/bin/b.rs
+++ abc217/src/bin/b.rs
@@ -1,16 +1,27 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        s: [String; 3],
+    }
+
+    if !s.contains(&String::from("ABC")) {
+        println!("ABC");
+    } else if !s.contains(&String::from("ARC")) {
+        println!("ARC");
+    } else if !s.contains(&String::from("AGC")) {
+        println!("AGC");
+    } else {
+        println!("AHC");
     }
 }
