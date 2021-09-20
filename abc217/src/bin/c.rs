a8775557c500ecff76a1c428a672363ca8b30c1b
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-09-20 23:34:24 +0900

    Feat: abc217 c

--- abc217/src/bin/c.rs
+++ abc217/src/bin/c.rs
@@ -1,16 +1,26 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        n: usize,
+        ps: [usize; n],
     }
+
+    let mut qs = vec![String::new(); n];
+
+    for (i, p) in ps.iter().enumerate() {
+        qs[p - 1] = format!("{}", i + 1);
+    }
+
+    println!("{}", qs.join(" "));
 }
