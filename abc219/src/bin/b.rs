762fb12cfddbc7c0930d20f7bd01fdbe5a50523f
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-09-21 20:35:31 +0900

    Feat: abc219 b

--- abc219/src/bin/b.rs
+++ abc219/src/bin/b.rs
@@ -1,16 +1,22 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        s: [String; 3],
+        t: String,
+    }
+
+    for c in t.chars() {
+        print!("{}", s[c.to_digit(10).unwrap() as usize - 1]);
     }
 }
