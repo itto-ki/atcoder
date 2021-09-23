ec56a3a1edac782b9ff99316c3e839552225afd4
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-09-23 23:19:34 +0900

    Feat: abc216 b

--- abc216/src/bin/b.rs
+++ abc216/src/bin/b.rs
@@ -1,16 +1,31 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        n: usize,
+        names: [String; 2 * n],
     }
+
+    let mut hset = HashSet::new();
+    let mut i = 0;
+    while i < 2 * n {
+        if !hset.contains(&format!("{} {}", names[i], names[i + 1])) {
+            hset.insert(format!("{} {}", names[i], names[i + 1]));
+            i += 2;
+        } else {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
