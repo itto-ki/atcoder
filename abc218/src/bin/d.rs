1a526026801c6aed5095fe0c581f4083b087f2f6
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-09-24 22:18:23 +0900

    Feat: abc219 c

--- abc218/src/bin/d.rs
+++ abc218/src/bin/d.rs
@@ -1,16 +1,20 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        n: usize,
+        xy: [(u32, u32); n],
     }
+
+    println!("{:?}", xy);
 }
--- abc219/src/bin/c.rs
+++ abc219/src/bin/c.rs
@@ -1,16 +1,40 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        x: String,
+        n: usize,
+        ss: [String; n],
+    }
+
+    let mut dict = HashMap::new();
+    for (i, c) in x.chars().enumerate() {
+        // +1をしないと、文字列がそこで終わっているのか、それともインデックス0の文字なのかが判定できずにおかしくなる
+        dict.insert(c, i + 1);
+    }
+
+    let mut orders = Vec::with_capacity(n);
+    for (i, s) in ss.iter().enumerate() {
+        let mut order = vec![0usize; 10];
+        for (j, c) in s.chars().enumerate() {
+            order[j] = *dict.get(&c).unwrap();
+        }
+        orders.push((order, i));
+    }
+
+    orders.sort();
+
+    for (_, i) in orders.iter() {
+        println!("{}", ss[*i]);
     }
 }
