09a16a6852f4be7f6b4c87a3fa5794b4d8d11050
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-09-24 22:50:59 +0900

    Feat: abc216 c

--- abc216/src/bin/c.rs
+++ abc216/src/bin/c.rs
@@ -1,16 +1,41 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        n: usize,
     }
+
+    let mut n0 = n;
+    let mut result = String::new();
+
+    // 問題設定とは逆に計算していく
+    loop {
+        // 残り1個になったら-1して終了
+        if n0 == 1 {
+            result.push('A');
+            break;
+        }
+
+        // 残りが奇数なら-1
+        if n0 % 2 == 1 {
+            result.push('A');
+            n0 -= 1
+        }
+
+        // 上記計算で偶数個になっているはずなので、2分の1する
+        n0 /= 2;
+        result.push('B');
+    }
+
+    println!("{}", result.chars().rev().collect::<String>());
 }
