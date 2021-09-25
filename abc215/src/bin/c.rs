e3916116b629e759dca8fc5d50c949b634c2fad3
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-09-25 18:55:36 +0900

    Feat: abc215 c

--- abc215/src/bin/c.rs
+++ abc215/src/bin/c.rs
@@ -1,16 +1,25 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        s: Chars,
+        k: usize,
     }
+
+    let result: BTreeSet<String> = (0..s.len())
+        .permutations(s.len())
+        .map(|x| x.iter().map(|&i| s[i]).collect())
+        .collect();
+
+    println!("{}", result.iter().nth(k - 1).unwrap());
 }
