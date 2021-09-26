c303356b01dbf531afd819255175a99f3bf164c3
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-09-26 19:47:38 +0900

    Feat: abc214 b

--- abc214/src/bin/b.rs
+++ abc214/src/bin/b.rs
@@ -1,16 +1,31 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        s: i64,
+        t: i64,
     }
+
+    let mut count = 0;
+    for a in 0..=100 {
+        for b in 0..=(100 - a) {
+            for c in 0..=(100 - (a + b)) {
+                if a + b + c <= s && a * b * c <= t {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", count);
 }
