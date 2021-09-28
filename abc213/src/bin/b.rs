42924c64899ee9bca83bbd63a6e151c2d7511f81
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-09-28 21:08:56 +0900

    Feat: abc213 b

--- abc213/src/bin/b.rs
+++ abc213/src/bin/b.rs
@@ -1,16 +1,29 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut b = a.clone();
+    b.sort();
+    let b: Vec<&usize> = b.iter().rev().collect();
+
+    for (i, x) in a.iter().enumerate() {
+        if b[1] == x {
+            println!("{}", i + 1);
+            break;
+        }
     }
 }
