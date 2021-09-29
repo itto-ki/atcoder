54397211afa91a412eec205df4f5c68f6997da49
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-09-29 21:32:10 +0900

    Feat: abc214 c

--- abc214/src/bin/c.rs
+++ abc214/src/bin/c.rs
@@ -17,5 +17,39 @@ fn main() {
         t: [usize; n],
     }
 
-    println!("{} {:?} {:?}", n, s, t);
+    // 高橋くんから最初に宝石を渡されるすぬけくんとその時の時間を選ぶ
+    let mut min = (0, t[0]);
+    for (i, x) in t.clone().into_iter().enumerate() {
+        if min.1 > x {
+            min = (i, x);
+        }
+    }
+
+    let mut t = t;
+
+    // 最初に宝石を渡されるすぬけくんからN番目のすぬけくんまでを計算
+    for i in (min.0)..n {
+        if min.0 != i {
+            if t[i - 1] + s[i - 1] < t[i] {
+                t[i] = t[i - 1] + s[i - 1];
+            }
+        }
+    }
+
+    // 1番目のすぬけくんから最初に宝石を渡されるすぬけくんまでを計算
+    for i in 0..(min.0) {
+        if i == 0 {
+            if t[n - 1] + s[n - 1] < t[i] {
+                t[i] = t[n - 1] + s[n - 1];
+            }
+        } else {
+            if t[i - 1] + s[i - 1] < t[i] {
+                t[i] = t[i - 1] + s[i - 1];
+            }
+        }
+    }
+
+    for x in t {
+        println!("{}", x);
+    }
 }
