ec2fa225e978073d29876f656fbaeee0efdf135c
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-10-04 07:54:48 +0900

    Feat: abc211 b

--- abc211/src/bin/b.rs
+++ abc211/src/bin/b.rs
@@ -1,16 +1,29 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        ss: [String; 4],
+    }
+
+    let set: HashSet<String> = vec!["H", "2B", "3B", "HR"]
+        .into_iter()
+        .map(|s| s.to_string())
+        .collect();
+    let input: HashSet<String> = ss.into_iter().collect();
+
+    if set == input {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
