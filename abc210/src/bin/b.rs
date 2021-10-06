a91933491377a6963e39fbb2be068b4299efc0dc
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-10-07 08:38:00 +0900

    Feat: abc210 b

--- abc210/src/bin/b.rs
+++ abc210/src/bin/b.rs
@@ -1,16 +1,30 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        _n: usize,
+        s: Chars,
+    }
+
+    for (i, c) in s.into_iter().enumerate() {
+        if c == '1' {
+            if i % 2 == 0 {
+                println!("Takahashi");
+                return;
+            } else {
+                println!("Aoki");
+                return;
+            }
+        }
     }
 }
