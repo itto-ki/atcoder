d60298ad11406e86c09dee53eb461ee8c3233c17
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-10-20 23:10:24 +0900

    Feat: abc209 b

--- abc209/src/bin/b.rs
+++ abc209/src/bin/b.rs
@@ -12,5 +12,23 @@ use superslice::*;
 
 fn main() {
     input!{
+        (n, x): (usize, i32),
+        a_lst: [i32; n],
+    }
+
+    let mut sum = 0;
+
+    for (i, a) in a_lst.iter().enumerate() {
+        if i % 2 != 0 {
+            sum += a - 1;
+        } else {
+            sum += a;
+        }
+    }
+
+    if sum > x {
+        println!("No");
+    } else {
+        println!("Yes");
     }
 }
