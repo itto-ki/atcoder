f056a78960dc3442a3e5107d6516dc32c33017e3
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-11-01 21:53:24 +0900

    Feat: abc208 b

--- abc208/src/bin/b.rs
+++ abc208/src/bin/b.rs
@@ -12,5 +12,24 @@ use superslice::*;
 
 fn main() {
     input!{
+        p: usize,
     }
+
+    let mut coins = Vec::with_capacity(10);
+    let mut factorial = 1;
+    for i in 0..10 {
+        factorial *= i + 1;
+        coins.push(factorial);
+    }
+
+    let mut remain = p;
+    let mut sum = 0;
+    for coin in coins.into_iter().rev() {
+        if remain / coin > 0 {
+            sum += remain / coin;
+            remain %= coin;
+        }
+    }
+
+    println!("{}", sum);
 }
