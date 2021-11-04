230705ac12fc49c64f987b33c7cb48faebf39b27
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-11-05 00:45:39 +0900

    Feat: abs 4/10

--- abs/src/bin/abc087b.rs
+++ abs/src/bin/abc087b.rs
@@ -1,16 +1,33 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        a: u64,
+        b: u64,
+        c: u64,
+        x: u64,
     }
+
+    let mut count = 0;
+    for p in 0..=a {
+        for q in 0..=b {
+            for r in 0..=c {
+                if 500 * p + 100 * q + 50 * r == x {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", count);
 }
