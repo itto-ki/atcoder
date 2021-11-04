33ccc7747829376cffccd9b81909c164cbd5cd1b
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-11-04 22:50:22 +0900

    Feat: abs 3/10

--- abs/src/bin/abc081b.rs
+++ abs/src/bin/abc081b.rs
@@ -1,16 +1,30 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        n: usize,
+        a_lst: [usize; n],
     }
+
+    let mut a_lst = a_lst;
+    let mut count = 0;
+    loop {
+        if a_lst.iter().any(|a| a % 2 == 1) {
+            break;
+        }
+        a_lst = a_lst.iter().map(|a| a / 2).collect();
+        count += 1;
+    }
+
+    println!("{}", count);
 }
