24b93da84d91ad646d6a11803659500c3d862bb0
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-11-07 17:57:18 +0900

    Feat: abs 6/10

--- abs/src/bin/abc088b.rs
+++ abs/src/bin/abc088b.rs
@@ -1,16 +1,35 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        n: usize,
+        alist: [u64; n],
     }
+
+    let mut alist = alist;
+    alist.sort();
+    let alist = alist.into_iter().rev().collect::<Vec<u64>>();
+    let alice = alist
+        .iter()
+        .enumerate()
+        .filter(|&(i, _)| i % 2 == 0)
+        .map(|(_, x)| x)
+        .sum::<u64>();
+    let bob = alist
+        .iter()
+        .enumerate()
+        .filter(|&(i, _)| i % 2 == 1)
+        .map(|(_, x)| x)
+        .sum::<u64>();
+    println!("{}", alice - bob);
 }
