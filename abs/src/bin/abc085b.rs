7a89e7d0df7d04d89e8a8c9cf172fb8d52584e9f
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-11-07 18:04:27 +0900

    Feat: abs 7/10

--- abs/src/bin/abc085b.rs
+++ abs/src/bin/abc085b.rs
@@ -1,16 +1,21 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        n: usize,
+        dlist: [u64; n],
     }
+
+    let set: HashSet<u64> = dlist.into_iter().collect();
+    println!("{}", set.len());
 }
