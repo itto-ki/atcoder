5518e8350de5f5d5fc19dfe9c0827ca5ac5c8188
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-11-07 17:27:27 +0900

    Feat: abs 5/10

--- abs/src/bin/abc083b.rs
+++ abs/src/bin/abc083b.rs
@@ -1,16 +1,35 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        (n, a, b): (u64, u64, u64),
     }
+
+    let mut sum = 0;
+    for x in 1..n + 1 {
+        let mut y = x;
+        let mut digit_sum = 0;
+        loop {
+            digit_sum += y % 10;
+            y /= 10;
+            if y < 1 {
+                break;
+            }
+        }
+        if a <= digit_sum && digit_sum <= b {
+            sum += x;
+        }
+    }
+
+    println!("{}", sum);
 }
