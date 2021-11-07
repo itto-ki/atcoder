8ac1a52aa331fe610ed3131802c4c09718217edd
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2021-11-07 18:24:46 +0900

    Feat: abs 8/10

--- abs/src/bin/abc085c.rs
+++ abs/src/bin/abc085c.rs
@@ -1,16 +1,32 @@
 #![allow(unused_imports)]
+use itertools::__std_iter::once;
+use itertools::*;
+use itertools_num::ItertoolsNum;
+use proconio::marker::*;
+use proconio::*;
 use std::cmp::*;
 use std::collections::*;
 use std::io::Write;
 use std::ops::Bound::*;
-use itertools::*;
-use itertools::__std_iter::once;
-use itertools_num::ItertoolsNum;
-use proconio::*;
-use proconio::marker::*;
 use superslice::*;
 
 fn main() {
-    input!{
+    input! {
+        (n, y): (usize, usize),
     }
+
+    for i in 0..=n {
+        for j in 0..=(n - i) {
+            if 10_000 * i + 5_000 * j + 1_000 * (n - i - j) < y {
+                continue;
+            }
+            let k = n - i - j;
+            if 10_000 * i + 5_000 * j + 1_000 * k == y {
+                println!("{} {} {}", i, j, k);
+                return;
+            }
+        }
+    }
+
+    println!("-1 -1 -1");
 }
